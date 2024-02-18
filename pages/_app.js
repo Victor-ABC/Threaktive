840c9ee30cdf69dfacc017f49bae6f49706d83d6
Alexander Euler <alexandereuler@t-online.de>
2024-02-18 13:46:17 +0100

    Adds more content

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,44 @@
 /* eslint-disable react/prop-types */
 import React from "react"
 import "../styles/global.css"
-import Script from "next/script"
+import { useEffect } from "react"
+import { Router } from "next/router"
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Script src="/scripts/script.js" defer />
-    </>
-  )
+  useEffect(() => {
+    /** Adds line numbers the provided code block. */
+    const addLineNumbers = function (codeElement) {
+      const lines = codeElement.getElementsByClassName("line")
+      console.log("lines", lines)
+
+      for (const [index, line] of [...lines].entries()) {
+        const lineNumber = document.createElement("span")
+        lineNumber.classList.add("line-number")
+        lineNumber.textContent = `${index + 1}`
+        line.prepend(lineNumber)
+      }
+    }
+
+    const addAllLineNumbers = function () {
+      const codeElements = document.querySelectorAll("pre code")
+
+      for (const element of codeElements) {
+        addLineNumbers(element)
+      }
+    }
+
+    addAllLineNumbers()
+
+    let lastUrl = null
+    Router.events.on("routeChangeComplete", (url) => {
+      if (lastUrl === url) {
+        return
+      }
+
+      lastUrl = url
+      addAllLineNumbers()
+    })
+  }, [])
+
+  return <Component {...pageProps} />
 }
